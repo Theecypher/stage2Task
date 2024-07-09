30afd238250aec8d361f2013dfb1aac5f98e298b
Theecypher <obioramma12@gmail.com>
2024-07-09 21:14:36 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { Route, Router, Routes } from "react-router";
-import Footer from "./Pages/Footer/Footer";
 import Header from "./Pages/Header/Header";
 import Navbar from "./Pages/Navbar/Navbar";
 import ProductPage from "./Pages/ProductPage/ProductPage";
 import ProductDisplay from "./Pages/ProductDisplay/ProductDisplay";
 import Cart from "./Pages/Cart/Cart";
 import FooterMobile from "./Pages/Footer/FooterMobile";
+import Footer from "./Pages/Footer/Footer";
 
 const App = () => {
   return (
-    // <div>
-    //   <Navbar />
-    //   <ProductPage />
-    //   <Footer />
-    // </div>
-
     <div>
       <Navbar />
       <Routes>
