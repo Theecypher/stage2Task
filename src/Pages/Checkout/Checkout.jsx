6f90c781158fb52bef306e5153492f94882f72ca
Theecypher <obioramma12@gmail.com>
2024-07-10 12:23:27 +0100

    initial commit

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -1,20 +1,22 @@
 import CheckoutForm from "./CheckoutForm.";
+import round from "../../assets/round.svg"
+import round1 from "../../assets/round2.png"
 
 const Checkout = () => {
     return ( 
         <div className="my-8">
-            <h3 className="text-center text-2xl my-7 text-[#081142] font-bold">Checkout</h3>
+            <h3 className="text-center text-[18px] my-7 text-[#000] md:text-3xl lg:text-5xl font-bold">Checkout</h3>
 
             <div>
-                <div className="flex mt-3 mb-5 items-center justify-center gap-2">
-                    <div className="bg-[#081142] h-5 w-5 rounded-full"></div>
-                    <p>Shipping</p>
-                    <p className="w-[100px] h-[2px] bg-[#081142]"></p>
-                    <div className="bg-[#8a94cc] h-5 w-5 rounded-full"></div>
-                    <p>Payment Details</p>
-                    <p className="w-[100px] h-[2px] bg-[#081142]"></p>
-                    <div className="bg-[#8a94cc] h-5 w-5 rounded-full"></div>
-                    <p>Confirmation</p>
+                <div className="flex mt-3 mb-5 items-center justify-center gap-[1px]">
+                    <img src={round} alt="" />
+                    <p className="text-[8px] md:text-[18px] lg:text-[30px]">Shipping</p>
+                    <p className="w-[62px] md:w-[100px ] lg:w-[200px]  h-[1px] bg-[#081142]"></p>
+                    <img src={round1} alt="" />
+                    <p className="text-[8px] md:text-[18px] lg:text-[30px]">Payment Details</p>
+                    <p className="w-[62px] md:w-[100px] lg:w-[200px] h-[1px] bg-[#081142]"></p>
+                    <img src={round1} alt="" />
+                    <p className="text-[8px] md:text-[18px] lg:text-[30px]">Confirmation</p>
                 </div>
             </div>
 
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -2,14 +2,15 @@ import { Link, NavLink } from "react-router-dom";
 import { Navlinks } from "./Navlinks";
 import cart from "../../assets/cart.svg";
 import search from "../../assets/search.svg";
+import contact from "../../assets/Vector1.png";
 import { AiOutlineSearch } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center bg-[#081142] text-[#fff] px-3 md:px-8 py-[10px] md:py-[45px]">
+    <div className="flex justify-between items-center bg-[#081142] text-[#fff] px-3 md:px-8 py-[10px] md:py-[25px]">
       <Link className="italic text-xs md:text-xl">TechDepot</Link>
 
-      <div className="bg-white flex md:hidden items-center">
+      <div className="bg-white flex items-center justify-between md:px-5 py-[5px]">
         <input
           type="text"
           className=" placeholder-black outline-none border-none"
@@ -19,21 +20,11 @@ const Navbar = () => {
         <AiOutlineSearch className="text-black font-extralight" />
       </div>
 
-      <div className="hidden md:flex justify-between">
-        {Navlinks.map((item, index) => (
-          <ul key={index} className="flex items-center">
-            <li className="px-2 text-xs md:text-base">
-              <Link to={item.to}>{item.title}</Link>
-            </li>
-          </ul>
-        ))}
-      </div>
-
       <div className="flex gap-[10px] justify-between">
-        <img src={search} sizes="18" alt="" className="hidden md:block" />
         <NavLink to="cart">
-          <img src={cart} sizes="18" alt="" />
+          <img className="md:w-" src={cart} sizes="18" alt="" />
         </NavLink>
+        <img src={contact} alt="" className="hidden md:block w-3" />
       </div>
     </div>
   );
